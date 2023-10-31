310e385e38d7a25d42937fe5ff43ddc44c9b0caf
Hamza Kaderi <kaderihamza4@gmail.com>
2023-10-31 15:06:18 -0700

    added post method in order to take in input from the user

--- express_service.js
+++ express_service.js
@@ -9,11 +9,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const generateRandomString = function () {
+  const shortURL = Math.random().toString(34).substring(7)
+  return shortURL;
+}
+
+console.log(generateRandomString())
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -27,6 +35,16 @@ app.get("/", (req, res) => {
 app.get("/hello", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_new", templateVars)
+})
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req, res) => {
